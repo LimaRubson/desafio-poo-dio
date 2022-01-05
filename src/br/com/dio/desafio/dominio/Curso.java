cb8f42ca5b2596ef5a6e0b0aeccf6d0de4e2994e
LimaRubson <limarubson@hotmail.com>
2022-01-05 15:57:14 -0300

    Final

--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -2,28 +2,15 @@ package br.com.dio.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Curso {
-    private String titulo;
-    private String descrição;
+public class Curso extends Conteudo {
     private int cargaHoraria;
 
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO + cargaHoraria;
+    }
+
     public Curso() {
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescrição() {
-        return descrição;
-    }
-
-    public void setDescrição(String descrição) {
-        this.descrição = descrição;
     }
 
     public int getCargaHoraria() {
@@ -37,9 +24,11 @@ public class Curso {
     @Override
     public String toString() {
         return "Curso{" +
-                "titulo='" + titulo + '\'' +
-                ", descrição='" + descrição + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descrição='" + getDescrição() + '\'' +
                 ", cargaHoraria=" + cargaHoraria +
                 '}';
     }
+
+
 }
